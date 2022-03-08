57f7a38eb01643cf9f7c4c90fed0f87cee5c5531
Robin Bedemann <robin.bedemann@gmail.com>
2022-03-08 13:52:20 +0100

    feat(ui): Define default route as /books

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BookList } from './list';
 import { Layout } from './layout';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { NotFound } from './NotFound';
 
 const App: React.VoidFunctionComponent = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<BookList />} />
+        <Route index element={<Navigate to="/books" replace />} />
+        <Route path={'books'} element={<BookList />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
